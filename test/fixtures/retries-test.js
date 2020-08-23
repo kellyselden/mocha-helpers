d5058441622aa2def181b4d793b4c69ed3d8fd36
Kelly Selden <kellyselden@gmail.com>
2020-08-23 15:34:23 +0100

    add tests for retry hooks

---
+++ test/fixtures/retries-test.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const mocha = {
+  exports: {}
+};
+
+require('../..')(mocha, {
+  retryHooks: true
+});
+
+const { before, beforeEach, afterEach, after } = mocha.exports;
+const assert = require('assert');
+
+describe('retries', function() {
+  describe('before', function() {
+    before(function() {
+      this.attempt = 0;
+    });
+
+    // eslint-disable-next-line mocha/no-sibling-hooks
+    before(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+    });
+
+    it('works', function() {
+      assert.ok(true);
+    });
+  });
+
+  describe('beforeEach', function() {
+    before(function() {
+      this.attempt = 0;
+    });
+
+    beforeEach(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+    });
+
+    it('works', function() {
+      assert.ok(true);
+    });
+  });
+
+  describe('it', function() {
+    before(function() {
+      this.attempt = 0;
+    });
+
+    it('works', function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+    });
+  });
+
+  describe('afterEach', function() {
+    before(function() {
+      this.attempt = 0;
+    });
+
+    afterEach(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+    });
+
+    it('works', function() {
+      assert.ok(true);
+    });
+  });
+
+  describe('after', function() {
+    before(function() {
+      this.attempt = 0;
+    });
+
+    after(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+    });
+
+    it('works', function() {
+      assert.ok(true);
+    });
+  });
+
+  describe('multiple', function() {
+    before(function() {
+      this.attempt = 0;
+    });
+
+    // eslint-disable-next-line mocha/no-sibling-hooks
+    before(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+      this.attempt = 0;
+    });
+
+    beforeEach(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+      this.attempt = 0;
+    });
+
+    it('works', function() {
+      assert.ok(true);
+    });
+
+    afterEach(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+      this.attempt = 0;
+    });
+
+    after(function() {
+      assert.strictEqual(this.attempt++, global.FAILURE_COUNT);
+      this.attempt = 0;
+    });
+  });
+});
