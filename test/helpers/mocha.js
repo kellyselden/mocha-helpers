5e08a819688d128d1fd2f5b3136927ede562ff51
Kelly Selden <kellyselden@gmail.com>
2020-08-24 09:47:41 +0100

    expose `setUpRunner` and `runTests`

--- test/helpers/mocha.js
+++ test/helpers/mocha.js
@@ -2,38 +2,4 @@
 
 require('../..')(module);
 
-const Mocha = require('mocha');
-const clearModule = require('clear-module');
-
-async function runTests(files, options) {
-  let mocha = new Mocha(options);
-
-  for (let file of files) {
-    mocha.addFile(file);
-  }
-
-  let runner;
-
-  await new Promise(resolve => {
-    // `mocha.run` is synchronous if no tests were found,
-    // otherwise, it's asynchronous...
-    runner = mocha.run(resolve);
-  });
-
-  return runner.stats;
-}
-
-function setUpRunner() {
-  Mocha.before(function() {
-    this.runTests = runTests;
-  });
-
-  Mocha.afterEach(function() {
-    // unfortunately, mocha caches previously run files,
-    // even though it is a new instance...
-    // https://github.com/mochajs/mocha/blob/v6.2.0/lib/mocha.js#L334
-    clearModule.all();
-  });
-}
-
-module.exports.setUpRunner = setUpRunner;
+module.exports.setUpRunner = require('../..').setUpRunner;
