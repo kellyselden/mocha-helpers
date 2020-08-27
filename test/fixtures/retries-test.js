513d5cc3931247fc09d4d63f6c96d8a01e45b262
Kelly Selden <kellyselden@gmail.com>
2020-08-27 20:25:36 +0100

    also retry hooks on timeouts

--- test/fixtures/retries-test.js
+++ test/fixtures/retries-test.js
@@ -98,8 +98,125 @@ describe('retries', function() {
         this.attempt = 0;
       });
     });
+  });
 
-    describe('timeout', function() {
+  describe('timeouts', function() {
+    this.timeout(10);
+
+    describe('before', function() {
+      before(function() {
+        this.attempt = 0;
+      });
+
+      before(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        }
+      });
+
+      it('works', function() {
+        assert.ok(true);
+      });
+    });
+
+    describe('beforeEach', function() {
+      before(function() {
+        this.attempt = 0;
+        console.log('before');
+      });
+
+      beforeEach(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          console.log('before timeout');
+          await new Promise(resolve => setTimeout(resolve, 15));
+          console.log('after timeout');
+        } else {
+          console.log('no timeout');
+        }
+      });
+
+      it('works', function() {
+        assert.ok(true);
+      });
+    });
+
+    describe('afterEach', function() {
+      before(function() {
+        this.attempt = 0;
+      });
+
+      afterEach(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        }
+      });
+
+      it('works', function() {
+        assert.ok(true);
+      });
+    });
+
+    describe('after', function() {
+      before(function() {
+        this.attempt = 0;
+      });
+
+      after(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        }
+      });
+
+      it('works', function() {
+        assert.ok(true);
+      });
+    });
+
+    describe('multiple', function() {
+      before(function() {
+        this.attempt = 0;
+      });
+
+      before(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        } else {
+          this.attempt = 0;
+        }
+      });
+
+      beforeEach(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        } else {
+          this.attempt = 0;
+        }
+      });
+
+      it('works', function() {
+        assert.ok(true);
+      });
+
+      afterEach(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        } else {
+          this.attempt = 0;
+        }
+      });
+
+      after(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        } else {
+          this.attempt = 0;
+        }
+      });
+    });
+  });
+
+  describe('errors and timeouts', function() {
+    describe('resets timeout', function() {
       this.timeout(15);
 
       before(function() {
@@ -114,6 +231,42 @@ describe('retries', function() {
       it('works', function() {
         assert.ok(true);
       });
+    });
+
+    describe('error after timeout', function() {
+      this.timeout(10);
+
+      before(function() {
+        this.attempt = 0;
+      });
+
+      beforeEach(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        } else {
+          assert.ok(false);
+        }
+      });
+
+      it('works', function() {});
+    });
+
+    describe('time out after error', function() {
+      this.timeout(10);
+
+      before(function() {
+        this.attempt = 0;
+      });
+
+      beforeEach(async function() {
+        if (this.attempt++ < global.FAILURE_COUNT) {
+          assert.ok(false);
+        } else {
+          await new Promise(resolve => setTimeout(resolve, 15));
+        }
+      });
+
+      it('works', function() {});
     });
   });
 });
