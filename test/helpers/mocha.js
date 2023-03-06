2577ecf22a54f208b3036872c2b67aca48ee42a5
Kelly Selden <kellyselden@gmail.com>
2023-03-06 14:28:21 -0800

    chore: give access to runner promise

--- test/helpers/mocha.js
+++ test/helpers/mocha.js
@@ -4,7 +4,7 @@ require('../..')(module);
 
 const Mocha = require('mocha');
 
-async function runTests(files, options) {
+function runTests(files, options) {
   let mocha = new Mocha({
     timeout: this._runnable._timeout,
     ...options
@@ -16,24 +16,31 @@ async function runTests(files, options) {
 
   let runner;
 
-  try {
-    await new Promise((resolve, reject) => {
-      try {
-        // `mocha.run` is synchronous if no tests were found,
-        // otherwise, it's asynchronous...
-        runner = mocha.run(resolve);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  } finally {
-    // unfortunately, mocha caches previously run files,
-    // even though it is a new instance...
-    // https://github.com/mochajs/mocha/blob/v6.2.0/lib/mocha.js#L334
-    mocha.unloadFiles();
-  }
+  let promise = (async() => {
+    try {
+      await new Promise((resolve, reject) => {
+        try {
+          // `mocha.run` is synchronous if no tests were found,
+          // otherwise, it's asynchronous...
+          runner = mocha.run(resolve);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    } finally {
+      // unfortunately, mocha caches previously run files,
+      // even though it is a new instance...
+      // https://github.com/mochajs/mocha/blob/v6.2.0/lib/mocha.js#L334
+      mocha.unloadFiles();
+    }
 
-  return runner.stats;
+    return runner.stats;
+  })();
+
+  return {
+    promise,
+    runner
+  };
 }
 
 module.exports.runTests = runTests;
--- test/integration/retries-test.js
+++ test/integration/retries-test.js
@@ -24,7 +24,7 @@ describe(function() {
           grep,
           ...options
         }
-      );
+      ).promise;
     };
 
     retryEventSpy = sinon.spy();
